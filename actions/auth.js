7612854b72110d1528e138c2bf396f52291d64fc
Janika <janika.liiv@gmail.com>
2016-04-09 18:05:04 +0300

    auth request returns existing user

--- actions/auth.js
+++ actions/auth.js
@@ -4,12 +4,19 @@ const userAuth = (mongoClient) => {
   const usersCollection = mongoClient.collection('users');
 
   function postFn(req, res) {
-    let user = {phone: req.body.phone, key: new ObjectID()}
-    usersCollection.insertOneAsync(user).then((result) => {
-      res.status(200).json(user);
+    usersCollection.findOneAsync({phone: req.body.phone}).then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        let user = {phone: req.body.phone, key: new ObjectID()}
+        usersCollection.insertOneAsync(user).then((result) => {
+          res.status(200).json(result);
+        }, (err) => {
+          res.status(400).json({error_message: 'problem inserting user'});
+        });
+      }
     }, (err) => {
-      console.log(err);
-      res.status(400).json({error_message: 'problem inserting user'});
+      res.status(400).json({error_message: 'error on creating user'});
     });
   };
 
